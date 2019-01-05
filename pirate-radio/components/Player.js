9f4c2017f41c82d639f6dc0bb2eab8bc07dbc0ce
Dawson Ley <Anarchonist@protonmail.com>
2019-01-04 16:27:14 -0800

    preparing to test perfect pause cross computer

--- pirate-radio/components/Player.js
+++ pirate-radio/components/Player.js
@@ -76,12 +76,12 @@ export default class Player extends Component {
 
       ///---------------------below is what's important
       if (status.positionMillis === this.state.totalLength) {
-      //   this.state.player.pauseAsync();
-      //     this.setState({
-      //       totalLength: this.state.totalLength + 2
-      //     }, () => {
-      //       this.props.sendMessage(this.state.selectedTrack + 1, Date.now())
-      //     })
+        this.state.player.pauseAsync();
+          this.setState({
+            totalLength: this.state.totalLength + 2
+          }, () => {
+            this.props.sendMessage(this.state.selectedTrack + 1, Date.now())
+          })
           //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^for captain
 
           // console.log(this.state.selectedTrack, this.state.tracks.length - 1)
@@ -137,7 +137,7 @@ export default class Player extends Component {
           var date = Date.now();
           // console.log('-----HERES YUR ENCHILADA: ', status.positionMillis, date, stamp)
           console.log(this.state.selectedTrack)
-          // this.props.updateCurrentTrack(this.state.selectedTrack, stamp, status.positionMillis, this.state.paused, (!this.state.sync))
+          this.props.updateCurrentTrack(this.state.selectedTrack, stamp, status.positionMillis, this.state.paused, (!this.state.sync))
         })
     }
   }
@@ -201,8 +201,8 @@ export default class Player extends Component {
           player: new Expo.Audio.Sound(),
           selectedTrack: this.props.ship.currentTrack,
         }, () => {
-          // this.props.updateCurrentTrack(this.state.selectedTrack, 0)
-          // setTimeout(() => {this.props.sendMessage('play', Date.now())}, 2000);
+          this.props.updateCurrentTrack(this.state.selectedTrack, 0)
+          setTimeout(() => {this.props.sendMessage('play', Date.now())}, 2000);
         });
       } else {
         this.state.player.setPositionAsync(0).then(() => {
@@ -226,8 +226,8 @@ export default class Player extends Component {
           player: new Expo.Audio.Sound(),
           selectedTrack: this.props.ship.currentTrack,
         }, () => {
-          // this.props.updateCurrentTrack(this.state.selectedTrack, 0);
-          // setTimeout(() => {this.props.sendMessage('play', Date.now())}, 2000);
+          this.props.updateCurrentTrack(this.state.selectedTrack, 0);
+          setTimeout(() => {this.props.sendMessage('play', Date.now())}, 2000);
         })
       }
     }
@@ -253,7 +253,11 @@ export default class Player extends Component {
                 console.log('---this the lag!: ', ((Date.now() - this.props.CT) / 2) + (Date.now() - this.props.ST))
 
       this.setState({paused: false});
-      setTimeout(() => {this.state.player.playAsync();}, 1000 - ((Date.now() - this.props.CT) / 2) + (Date.now() - this.props.ST))
+      setTimeout(() => {
+        this.state.player.setPositionAsync(this.props.ship.currentPositionMillis).then(() => {
+          this.state.player.playAsync();
+        })
+      }, 1000 - ((Date.now() - this.props.CT) / 2) + (Date.now() - this.props.ST))
     }
     return (
       <View>
