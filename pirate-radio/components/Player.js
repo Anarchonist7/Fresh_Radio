72dac7d6ab1db1fb8095091f9cea325896972bf7
Dawson Ley <Anarchonist@protonmail.com>
2019-01-06 18:47:41 -0800

    captain's stuff commented in

--- pirate-radio/components/Player.js
+++ pirate-radio/components/Player.js
@@ -76,12 +76,12 @@ export default class Player extends Component {
 
       ///---------------------below is what's important
       if (status.positionMillis === this.state.totalLength) {
-        // this.state.player.pauseAsync();
-        //   this.setState({
-        //     totalLength: this.state.totalLength + 2
-        //   }, () => {
-        //     this.props.sendMessage(this.state.selectedTrack + 1, Date.now())
-        //   })
+        this.state.player.pauseAsync();
+          this.setState({
+            totalLength: this.state.totalLength + 2
+          }, () => {
+            this.props.sendMessage(this.state.selectedTrack + 1, Date.now())
+          })
           //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^for captain
 
           // console.log(this.state.selectedTrack, this.state.tracks.length - 1)
@@ -137,7 +137,7 @@ export default class Player extends Component {
           var date = Date.now();
           // console.log('-----HERES YUR ENCHILADA: ', status.positionMillis, date, stamp)
           console.log(this.state.selectedTrack)
-          // this.props.updateCurrentTrack(this.state.selectedTrack, stamp, status.positionMillis, this.state.paused, (!this.state.sync))
+          this.props.updateCurrentTrack(this.state.selectedTrack, stamp, status.positionMillis, this.state.paused, (!this.state.sync))
         })
     }
   }
@@ -198,9 +198,9 @@ export default class Player extends Component {
       player: new Expo.Audio.Sound(),
       selectedTrack: this.props.ship.currentTrack
     }, () => {
-      // this.props.sendMessage('pause', Date.now(), 0);
-      // this.props.updateCurrentTrack(this.state.selectedTrack, 0, 0)
-      // setTimeout(() => {this.props.sendMessage('play', Date.now())}, 4500);
+      this.props.sendMessage('pause', Date.now(), 0);
+      this.props.updateCurrentTrack(this.state.selectedTrack, 0, 0)
+      setTimeout(() => {this.props.sendMessage('play', Date.now())}, 4500);
     });
   }
 
