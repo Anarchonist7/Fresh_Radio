78062732b836c457037607b225f241ec9452e1f2
Dawson Ley <Anarchonist@protonmail.com>
2019-01-08 13:16:40 -0800

    fixed seeds

--- simpleserver/db/seeds/dev/tracks.js
+++ simpleserver/db/seeds/dev/tracks.js
@@ -9,12 +9,12 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('songs').insert([
-        {id: 1, title: 'Rouge', artist: 'TOKiMONSTA', album: 'Lune Rouge', audio_url: 'https://s3-us-west-2.amazonaws.com/pirate-radio/tune1.mp3', duration_millis: 133198, ship_id: 1, order: 1},
-        {id: 2, title: 'Estrange (Feat. Io Echo)', artist: 'TOKiMONSTA', album: 'Lune Rouge', audio_url: 'https://s3-us-west-2.amazonaws.com/pirate-radio/tune2.mp3', duration_millis: 258011, ship_id: 1, order: 2},
-        {id: 3, title: 'Cirrus', artist: 'Bonobo', album: 'The North Borders', audio_url: 'https://s3-us-west-2.amazonaws.com/pirate-radio/tune3.mp3', duration_millis: 349257, ship_id: 1, order: 3},
-        {id: 4, title: 'Elevate This Sound', artist: 'Calyx, Teebee', album: 'Elevate This Sound', audio_url: 'https://s3-us-west-2.amazonaws.com/pirate-radio/tune4.mp3', duration_millis: 315637, ship_id: 1, order: 4},
+        {id: 1, title: 'Rouge', artist: 'TOKiMONSTA', album: 'Lune Rouge', audio_url: 'http://' + LOCALHOST + ':' + PORT + '/tune1.mp3', duration_millis: 133198, ship_id: 1, order: 1},
+        {id: 2, title: 'Estrange (Feat. Io Echo)', artist: 'TOKiMONSTA', album: 'Lune Rouge', audio_url: 'http://' + LOCALHOST + ':' + PORT + '/tune2.mp3', duration_millis: 258011, ship_id: 1, order: 2},
+        {id: 3, title: 'Cirrus', artist: 'Bonobo', album: 'The North Borders', audio_url: 'http://' + LOCALHOST + ':' + PORT + '/tune3.mp3', duration_millis: 349257, ship_id: 1, order: 3},
+        {id: 4, title: 'Elevate This Sound', artist: 'Calyx, Teebee', album: 'Elevate This Sound', audio_url: 'http://' + LOCALHOST + ':' + PORT + '/tune4.mp3', duration_millis: 315637, ship_id: 1, order: 4},
         {id: 5, title: 'Truth', artist: 'Alexander', album: 'Alexander', duration_millis: 260963, audio_url: 'http://' + LOCALHOST + ':' + PORT + '/03%20Truth.mp3', ship_id: 3, order: 5},
-        {id: 6, title: 'Ghost Ship', artist: 'Bonobo', album: 'Black Sands Remixed', duration_millis: 241789, audio_url: 'https://s3-us-west-2.amazonaws.com/pirate-radio/08+Ghost+Ship.mp3', ship_id: 1, order: 6},
+        {id: 6, title: 'Ghost Ship', artist: 'Bonobo', album: 'Black Sands Remixed', duration_millis: 241789, audio_url: 'http://' + LOCALHOST + ':' + PORT + '/08+Ghost+Ship.mp3', ship_id: 1, order: 6},
         {id: 7, title: "Captain Kelly's Kitchen", artist: 'Dropkick Murphys', album: "The Warrior's Code", duration_millis: 168881, audio_url: 'http://' + LOCALHOST + ':' + PORT + "/03%20Captain%20Kelly's%20Kitchen.mp3", ship_id: 2, order: 7},
         {id: 8, title: "I'm Shipping Up to Boston", artist: 'Dropkick Murphys', album: "The Warrior's Code", duration_millis: 166504, audio_url: 'http://' + LOCALHOST + ':' + PORT + "/11%20I'm%20Shipping%20Up%20to%20Boston.mp3", ship_id: 2, order: 8},
         {id: 9, title: 'Salty Dog', artist: 'Flogging Molly', album: 'Swagger', duration_millis: 141453, audio_url: 'http://' + LOCALHOST + ':' + PORT + '/01%20Salty%20Dog.mp3', ship_id: 2, order: 9},
