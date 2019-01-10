0e36f04f35c3d4b8e5c23f2105043726f5686a0d
aaronrbg <aaronrbg@aaronrbg.com>
2019-01-10 00:10:30 -0800

    servers combined. app still yellow screenign lots

--- simpleserver/server.js
+++ simpleserver/server.js
@@ -12,6 +12,52 @@ const pirateDb = require('./lib/pirateDb')(knex);
 
 var mp3Duration = require('mp3-duration');
 
+/////////////////////////////////////////////////////////////////////////////////
+
+// var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
+
+// var app = express();
+var server = http.Server(app);
+var websocket = socketio(server);
+var damned = 0;
+//server.listen(3003, () => console.log('listening on *:3003'));
+
+// The event will be called when a client is connected.
+websocket.on('connection', (socket) => {
+  console.log('A client just joined on', socket.id);
+
+  socket.on('message', (message) => {
+    data = JSON.parse(message);
+    if (data.content === 'play') {
+      console.log('playing', Date.now());
+      console.log(Date.now())
+      websocket.send(JSON.stringify({type: 'message', content: false, CT: data.time, ST: Date.now()}));
+    } else if (data.content === 'pause') {
+      console.log('pausing', Date.now());
+      websocket.send(JSON.stringify({type: 'message', content: true, MS: data.MS, CT: data.time, ST: Date.now()}));
+    } else if (!isNaN(data.content)) {
+      console.log('moving');
+      websocket.send(JSON.stringify({type: 'next', content: data.content}));
+    } else if (data.content === 'ahoy!') {
+      console.log('we got a joiner!');
+      damned++;
+      websocket.send(JSON.stringify({type: 'count', content: damned}));
+    } else if (data.content === 'avast!') {
+      console.log('walk the plank ye scurvy dawg!');
+      damned--;
+      websocket.send(JSON.stringify({type: 'count', content: damned}));
+    } else if (data.content === 'request') {
+      console.log('arrr requesting sync mr.captain!');
+      websocket.send(JSON.stringify({type: 'request', content: 'request'}));
+    }
+  });
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
 app.use(express.static('public'))
 
 app.get("/ships/:id", function(req, res) {
@@ -125,3 +171,5 @@ app.post("/ships/:id", function(req, res) {
 app.listen(PORT, () => {
     console.log(`live on port http://${LOCALHOST}:${PORT}`);
   });
+
+server.listen(3003, () => console.log('listening on *:3003'));
